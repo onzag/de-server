7acf4e64a7a9a1f024f93ce761ee90a62b23d98e
onzag <ejgonzalezviera@gmail.com>
2026-03-04 10:27:41 +0200

    ?=??

--- scripts/Behemoth-123B-nvfp4.sh
+++ scripts/Behemoth-123B-nvfp4.sh
@@ -5,4 +5,4 @@
 # Will not run on a H100 GPU, and the H100 is not optimized for FP4, while the FP8 version is too large for the H100
 
 bash install-python-deps.sh
-./pyenv/bin/python download-model.sh Firworks/Behemoth-X-123B-v2.1-nvfp4
+./pyenv/bin/python download-model.py Firworks/Behemoth-X-123B-v2.1-nvfp4
